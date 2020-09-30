3a07097ab42611a4166abbe1e378550c861720f3
orenkoren <oren-k@hotmail.com>
2020-10-01 02:34:44 +0300

    fixed some errors

--- chatApp/src/app/login/login.component.ts
+++ chatApp/src/app/login/login.component.ts
@@ -7,9 +7,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: string;
 
   constructor(
     private router: Router,
+
   ) { }
 
   ngOnInit(): void {
